389587c3b5355a3c65c8bd8a49a38cda3e4f7075
Jaime <jaimebrs1979@gmail.com>
2026-04-19 21:44:48 -0500

    docker-compose.yml actualizado

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res){
-    return res.json({mensaje: "Saludos desde mi App de Node (Modificado)"});
+    return res.json({mensaje: "Saludos desde mi App de Node (Modificado) ajuste 2"});
 });
 
 app.listen(3000, function(){
-    console.log("Servidor corriendo en el puerto 3000");
-})
+    console.log("Servidor corriendo en el puerto 3000 ajuste 2");
+}) 
