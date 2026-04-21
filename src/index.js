61fbfba3a47d65e22f4b57016c9fa31da7efccbe
Jaime <jaimebrs1979@gmail.com>
2026-04-20 19:39:42 -0500

    Actualización del proyecto con conexion a bd y creación de tablas

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
-
+const User = require("./models/User");
+const Producto = require("./models/Producto");
 // inicializando una app de express
 const app = express();
 
 app.get("/", function(req, res){
-    return res.json({mensaje: "Saludos desde mi App de Node (Modificado) ajuste 2"});
+    return res.json({mensaje: "Saludos desde mi App de Node (MODIFICADO) último 3"});
 });
 
 app.listen(3000, function(){
-    console.log("Servidor corriendo en el puerto 3000 ajuste 2");
-}) 
+    console.log("Servidor corriendo en el puerto 3000 (ultima modificación) 3");
+})
